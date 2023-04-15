c18e23aa0ba11571c6aeff9a8317034e9b1d4c65
ajay0398 <ajay.mahato0398@gmail.com>
2023-04-15 23:54:44 +0530

    initial commit

---
+++ src/main/java/com/gtech/chopsy/ChopsyApplication.java
@@ -0,0 +1,13 @@
+package com.gtech.chopsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtech/chopsy/ChopsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtech.chopsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
