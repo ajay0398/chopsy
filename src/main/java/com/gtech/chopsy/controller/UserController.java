2041e0c7d429729bbb7b6f899ad3451f4d0ed862
ajay0398 <ajay.mahato0398@gmail.com>
2023-04-16 12:30:53 +0530

    added mysql dependencies and lombok removed statics userRoster

--- src/main/java/com/gtech/chopsy/controller/UserController.java
+++ src/main/java/com/gtech/chopsy/controller/UserController.java
@@ -2,7 +2,8 @@ package com.gtech.chopsy.controller;
 
 
 import com.gtech.chopsy.model.User;
-import com.gtech.chopsy.util.UserRoster;
+import com.gtech.chopsy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    UserRepository repository;
 
     @GetMapping()
     public List<User> getAllUser(){
-        return UserRoster.getAllUser();
+        //return UserRoster.getAllUser();
+        return repository.findAll();
     }
+
+
 
 }
--- src/main/java/com/gtech/chopsy/model/User.java
+++ src/main/java/com/gtech/chopsy/model/User.java
@@ -1,67 +1,27 @@
 package com.gtech.chopsy.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String mobileNumber;
     private String password;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User(Integer id, String firstName, String lastName, String email, String mobileNumber, String password) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.mobileNumber = mobileNumber;
-        this.password = password;
-    }
 }
